88331ece07ba03572d3664784ba51d2a4cc2f61a
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2019-04-18 20:25:30 +0300

    Created ObjectBox

---
+++ src/ObjectBox.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class ObjectBox {
+    private ArrayList<Object> objects;
+
+    public void addObject(Object object) {
+        this.objects.add(object);
+    }
+
+    public void deleteObject(Object object) {
+        this.objects.remove(object);
+    }
+
+    public String dump() {
+        String string = "[" + this.objects.get(0).toString();
+
+        for (int i = 1; i < this.objects.size(); i++) {
+            string += ", " + this.objects.get(i).toString();
+        }
+
+        string += "]";
+
+        return string;
+    }
+}
