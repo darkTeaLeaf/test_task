ccb0dcc90ee18f66c10d9e2e3fb6fec3cd9fef5b
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2019-04-18 20:10:36 +0300

    Created Mathbox

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Number[] array = {1, 5,5, 30, 6, 47};
+        MathBox box = new MathBox(array);
+
+        System.out.println(box.toString());
+
+        System.out.println(box.summator());
+
+        box.splitter(3);
+
+        System.out.println(box.toString());
+
+        box.delete(10);
+
+        System.out.println(box.toString());
+
+        Number[] array2 = {0, 1, 2, 15};
+        MathBox box2 = new MathBox(array2);
+
+        System.out.println(box.equals(box2));
+
+        System.out.println("Box hash: " + box.hashCode());
+        System.out.println("Box2 hash: " + box2.hashCode());
+    }
+}
