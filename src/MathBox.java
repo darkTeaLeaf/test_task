d0531134fd0299c7c3ef05491ccedea8c788f62b
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2019-04-18 21:07:56 +0300

    Fixed delete

--- src/MathBox.java
+++ src/MathBox.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class MathBox {
     ArrayList<Number> numbers;
@@ -26,31 +25,22 @@ public class MathBox {
 
     public void splitter(double divider) {
         for (int i = 0; i < this.numbers.size(); i++) {
-            int previous = this.numbers.get(i).intValue();
-            this.numbers.set(i, (int)(previous / divider));
-        }
-        for (int i = 0; i < this.numbers.size(); i++) {
             if (this.numbers.get(i) instanceof Double || this.numbers.get(i) instanceof Float) {
                 int previous = this.numbers.get(i).intValue();
-                this.numbers.set(i, previous / divider);
+                this.numbers.set(i, (double)previous / divider);
             } else {
                 long previous = this.numbers.get(i).longValue();
-                this.numbers.set(i, (long)(previous / divider));
+                this.numbers.set(i, (long) (previous / divider));
             }
         }
     }
 
-    public void delete(Integer value){
-        for (int i = 0; i < this.numbers.size(); i++) {
-            if(value.equals(this.numbers.get(i))){
-                this.numbers.remove(i);
-                break;
-            }
-        }
+    public void delete(Integer value) {
+        this.numbers.remove(value);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String object = "[" + this.numbers.get(0);
 
         for (int i = 1; i < this.numbers.size(); i++) {
@@ -63,13 +53,13 @@ public class MathBox {
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         if (this.numbers == null)
             return 0;
 
         int result = 1;
         for (Number element : this.numbers) {
-            int elementHash = (int)(element.longValue() ^ (element.longValue() >>> 32));
+            int elementHash = (int) (element.longValue() ^ (element.longValue() >>> 32));
             result = 31 * result + elementHash;
         }
 
@@ -77,7 +67,7 @@ public class MathBox {
     }
 
     @Override
-    public boolean equals(Object obj){
+    public boolean equals(Object obj) {
         if (obj == this) {
             return true;
         }
@@ -87,13 +77,13 @@ public class MathBox {
         }
 
         MathBox other = (MathBox) obj;
-        if(other.numbers.size() == this.numbers.size()) {
+        if (other.numbers.size() == this.numbers.size()) {
             for (int i = 0; i < other.numbers.size(); i++) {
-                if(!other.numbers.get(i).equals(this.numbers.get(i))){
+                if (!other.numbers.get(i).equals(this.numbers.get(i))) {
                     return false;
                 }
             }
-        }else {
+        } else {
             return false;
         }
 
